3b4ccb141d412fffc89c2d80cabb59ca084ce38e
abdisag1 <myabdisa@gmail.com>
2022-07-11 20:51:35 +0300

    task 1

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 module.exports = {
   mode: "production", // "production" | "development" | "none"
-  entry: "./js/dashboard_main.js", // string | object | array
+  entry:{ 
+    main: path.resolve(__dirname, './js/dashboard_main.js'), // string | object | array
   // defaults to ./src
   // Here the application starts executing
   // and webpack starts bundling
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
